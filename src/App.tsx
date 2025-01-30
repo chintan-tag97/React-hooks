7db61ea46244b3e3f4775a99a7b53432f491a403
chintan-tag97 <chintan@tag97.com>
2025-01-30 14:49:31 +0530

    uploading

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Hooks from './Hooks.tsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Hooks/>
+    </div>
+  );
+}
+
+export default App;
